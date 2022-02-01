1b22e60e13d2cfbc0acf6b862113f173cfbb2123
Leddner <ada_hack442@hackademy.mx>
2022-02-01 02:39:47 -0600

    Ejercicio 1 resuelto

--- script.js
+++ script.js
@@ -3,6 +3,22 @@ const ejercicio1 = [
   19, 88, 456, 13, 23, 24,
 ];
 
+for ( let posicion = 0; posicion < ejercicio1.length; posicion++ ){
+  let num = ejercicio1[posicion];
+  let divisorestotales = 0;
+
+  for (let divisor = 1; divisor <= num; divisor++){
+    if (num % divisor === 0) {
+      divisorestotales++;
+    }
+  }
+
+  if (divisorestotales === 2) {
+    console.log(`El numero ${num} es primo`);
+  }
+}
+
+
 const ejercicio2 = [
   {
     nombre: "Gabriel",
